fafda3a3e0d4100826c1207e1e0f6897bd3b0851
Sanjay <sbs13082003@gmail.com>
2023-07-10 21:44:30 +0530

    Updating the landing page

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var navIcon = document.getElementById('nav-icon1');
-  var menu = document.querySelector(".menu-screen");
+var navIcon = document.getElementById('nav-icon1');
+var menu = document.querySelector(".menu-screen");
 
-  navIcon.addEventListener('click', function () {
-    navIcon.classList.toggle('open');
-    if (menu.classList.contains('open')) {
-      menu.classList.remove('open');
+navIcon.addEventListener('click', function () {
+  navIcon.classList.toggle('open');
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+  } else {
+    menu.classList.add('open');
+  }
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+     if (entry.isIntersecting) {
+      entry.target.classList.add('show');
     } else {
-      menu.classList.add('open');
+      entry.target.classList.remove('show');
     }
   });
 });
-
-function fadeItems() {
-  var items = document.getElementsByClassName("menu-items");
-
-  for (var i = 0; i < items.length; i++) {
-      setTimeout(function() {
-          items[i].classList.add("fadein");
-      }, 3000);
-  }
-}
+  
+const hiddenElements = document.querySelectorAll('.hidden') ;
+hiddenElements.forEach((el) => observer.observe(el));
